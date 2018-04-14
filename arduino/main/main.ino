06dd2ca2b0f30cf0a3e4bae6748fdb905dfcd16d
Vladimir Khrustalev <vovken1997@mail.ru>
2018-04-14 23:13:02 +0300

    def add

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,14 +1,33 @@
+#include <Servo.h> 
 
-#include <Servo.h> //используем библиотеку для работы с сервоприводом
-Servo servo; //объявляем переменную servo типа Servo
-void setup() //процедура setup
+#define LEFT_R( speed )   servo.write( 90 + (speed) );
+#define RIGHT_R( speed )  servo.write( 90 - (speed) );
+#define STOP()              servo.write( 90 );
+
+Servo servo; 
+
+#define DEF_SPEED   20
+#define NUMBER_PIN  10
+
+void setup() 
 {
-servo.attach(10); //привязываем привод к порту 10
-
+servo.attach( NUMBER_PIN ); 
 }
 
-void loop() //процедура loop
+void loop()
 {
+  LEFT_R( DEF_SPEED ); // write 1
+  delay( 50 );
+  RIGHT_R( DEF_SPEED ); //write 0
+  delay( 50 );
+  STOP();
+  delay( 50 );
+  LEFT_R( DEF_SPEED );
+  delay( 50 );
+  STOP();
+  delay( 50 );
+  
+  /*
   servo.write( 180 ); // write 1
   delay( 50 );
   servo.write( 0 ); // write 0
@@ -19,10 +38,10 @@ void loop() //процедура loop
   delay( 50 );
   servo.write( 90 );
   delay( 50 );
-  
+  */
 
 
-    delay( 20000 );
+    delay( 2000 );
   
 //servo.write(100); //ставим вал под 0
 
