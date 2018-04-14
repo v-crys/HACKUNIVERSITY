3f8389fd182fccaee827b43e2024426e135dd169
Vladimir Khrustalev <vovken1997@mail.ru>
2018-04-14 22:26:42 +0300

    servo worked

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,13 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
-  pinMode( 13, OUTPUT );
+
+#include <Servo.h> //используем библиотеку для работы с сервоприводом
+Servo servo; //объявляем переменную servo типа Servo
+void setup() //процедура setup
+{
+servo.attach(10); //привязываем привод к порту 10
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite( 13, LOW );
-  delay( 1000 );
-  digitalWrite( 13, HIGH );
-  delay( 1000 );
-}
+void loop() //процедура loop
+{
+  servo.write( 180 ); // write 1
+  delay( 50 );
+  servo.write( 0 ); // write 0
+  delay( 50 );
+  servo.write( 90 ); // 0
+  delay( 50 );
+  servo.write( 180 ); //1
+  delay( 50 );
+  servo.write( 90 );
+  delay( 50 );
+  
 
+
+    delay( 20000 );
+  
+//servo.write(100); //ставим вал под 0
+
+//delay(50000); //ждем 2 секунды
+//servo.write(170); //ставим вал под 180
+//delay(2000); //ждем 2 секунды
+}
